8efd659a7a74b7580a0d8ad993744f33f017c5b5
Marek Kolodziej <mkolod@gmail.com>
2017-05-06 17:39:33 +0000

    Web app placeholder

---
+++ seq2seq/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+ 
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+ 
+class ReusableForm(Form):
+    text = TextField('Text to be translated:', validators=[validators.required()])
+ 
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)
+ 
+    print form.errors
+    if request.method == 'POST':
+        text=request.form['text']
+        print text
+ 
+        if form.validate():
+            # Save the comment here.
+            flash('Translation: ' + text)
+        else:
+            flash('Error: All the form fields are required. ')
+ 
+    return render_template('translate.html', form=form)
+ 
+if __name__ == "__main__":
+    app.run()
